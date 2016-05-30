9b21f0fcb181e581c28e2d44f44cc96057ccf48c
kszela24 <kszela24@gmail.com>
2016-05-30 15:15:11 -0400

    Updating files, separating datasets into groups.
    Ran separate models on each of the 4 groups.

--- Szela/data-munging.R
+++ Szela/data-munging.R
@@ -1,15 +1,17 @@
+#Loading important packages.
 library(xgboost)
-library(caret)
-library(neuralnet)
 
-setwd("~/GitHub-kszela24/Higgs")
+setwd("~/GitHub-kszela24/higgs-bozon/Szela")
 
+#Reading in training and testing data.
 train = read.csv("training.csv")
 test = read.csv("test.csv")
 
+#Set -999's to NA for separation.
 train[train == -999] = NA
 test[test == -999] = NA
 
+#Separate into 4 separate groups of NA's
 train.NA.DER_mass_MMC = train[rowSums(is.na(train['DER_mass_MMC'])) > 0,]
 test.NA.DER_mass_MMC = test[rowSums(is.na(test['DER_mass_MMC'])) > 0,]
 
@@ -37,40 +39,15 @@ test.NA.DER_mass_MMC = test.NA.DER_mass_MMC[,colSums(is.na(test.NA.DER_mass_MMC)
 train.NA.PRI_jet_leading_pt = train.NA.PRI_jet_leading_pt[, colSums(is.na(train.NA.PRI_jet_leading_pt)) != nrow(train.NA.PRI_jet_leading_pt)]
 test.NA.PRI_jet_leading_pt = test.NA.PRI_jet_leading_pt[, colSums(is.na(test.NA.PRI_jet_leading_pt)) != nrow(test.NA.PRI_jet_leading_pt)]
 
-train.no.NA.w_l = train.no.NA[,c(32, 33)]
-train.no.NA$Weight = NULL
-train.no.NA$Label = NULL
+#Save each of the four separate groups to a csv for further processing.
+write.csv(train.no.NA, "train_no_NA.csv", row.names = F)
+write.csv(test.no.NA, "test_no_NA.csv", row.names = F)
 
-train.NA.PRI_jet_leading_pt.w_l = train.NA.PRI_jet_leading_pt[,c(22, 23)]
-train.NA.PRI_jet_leading_pt$Weight = NULL
-train.NA.PRI_jet_leading_pt$Label = NULL
+write.csv(train.NA.DER_mass_jet_jet, "train_NA_DER_mass_jet_jet.csv", row.names = F)
+write.csv(test.NA.DER_mass_jet_jet, "test_NA_DER_mass_jet_jet.csv", row.names = F)
 
-train.NA.DER_mass_jet_jet.w_l = train.NA.DER_mass_jet_jet[,c(25, 26)]
-train.NA.DER_mass_jet_jet$Weight = NULL
-train.NA.DER_mass_jet_jet$Label = NULL
+write.csv(train.NA.PRI_jet_leading_pt, "train_NA_PRI_jet_leading_pt.csv", row.names = F)
+write.csv(test.NA.PRI_jet_leading_pt, "test_NA_PRI_jet_leading_pt.csv", row.names = F)
 
-train.NA.DER_mass_MMC.w_l = train.NA.DER_mass_MMC[,c(31, 32)]
-train.NA.DER_mass_MMC$Weight = NULL
-train.NA.DER_mass_MMC$Label = NULL
-
-train.no.NA.index = train.no.NA$EventId
-test.no.NA.index = test.no.NA$EventId
-train.no.NA$EventId = NULL
-test.no.NA$EventId = NULL
-
-cat("\n## Removing the constants features.\n")
-for (f in names(train.no.NA)) {
-  if (length(unique(train.no.NA[[f]])) == 1) {
-    cat(f, "is constant in train. We delete it.\n")
-    train.no.NA[[f]] <- NULL
-    test.no.NA[[f]] <- NULL
-  }
-}
-
-
-train.no.NA$Target = train.no.NA.w_l$Weight
-train.no.NA.model <- sparse.model.matrix(Target ~ ., data = train.no.NA)
-
-
-
-
+write.csv(train.NA.DER_mass_MMC, "train_NA_DER_mass_MMC.csv", row.names = F)
+write.csv(test.NA.DER_mass_MMC, "test_NA_DER_mass_MMC.csv", row.names = F)
